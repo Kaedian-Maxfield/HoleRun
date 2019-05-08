21e4c0e4724fe85092ddb48f46d2d6d10ce0c247
Kaedian-Maxfield <38064751+Kaedian-Maxfield@users.noreply.github.com>
2019-05-08 09:12:51 -0600

    Player adjustment
    Less serialized requirements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] GameObject m_player = null;
-    [SerializeField] Rigidbody m_rb = null;
-    [SerializeField] float m_speed = 1.0f;
+    [SerializeField] float m_speed = 15.0f;
     [SerializeField] float m_jumpLength = 1.0f;
+    [SerializeField] float jumpForce = 5.0f;
+    Rigidbody m_rb = null;
     bool canJump = true;
     float timer = 0.0f;
-    float jumpForce = 5.0f;
 
     private void Start()
     {
         m_speed /= 100;
+        gameObject.AddComponent<Rigidbody>();
+        m_rb = gameObject.GetComponent<Rigidbody>();
+        m_rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
     // Update is called once per frame
@@ -22,11 +24,11 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            m_player.transform.Translate(Vector3.right * m_speed, Space.Self);
+            gameObject.transform.Translate(Vector3.right * m_speed, Space.Self);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            m_player.transform.Translate(Vector3.left * m_speed, Space.Self);
+            gameObject.transform.Translate(Vector3.left * m_speed, Space.Self);
         }
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && canJump && timer > 0.0f)
         {
