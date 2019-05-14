4eb86c6fd04858cdf89772cb615beccdb8bb6612
beastpuncher <beastpuncher@gmail.com>
2019-05-14 09:21:16 -0600

    Player Loses if the touch the black hole

--- Assets/Scripts/LoseArea.cs
+++ Assets/Scripts/LoseArea.cs
@@ -6,11 +6,16 @@ public class LoseArea : MonoBehaviour
 {
     public bool playerFallen = false;
 
+    [SerializeField] GameObject m_player = null;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             playerFallen = true;
+            m_player.GetComponent<Player>().enabled = false;
+            m_player.GetComponent<Rigidbody>().isKinematic = true;
+            Time.timeScale = 0.0f;
         }
     }
 }
