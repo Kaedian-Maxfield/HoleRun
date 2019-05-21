2cc06381de79d7778397074373af09df0cfbe575
Jesse Leal <38059219+JesseLeal@users.noreply.github.com>
2019-05-21 09:15:12 -0600

    Main Menu Scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject m_howToPlay = null;
+    [SerializeField] float m_rotSpeed = 0.125f;
+
+    private void Update()
+    {
+        Camera.main.transform.Rotate(Vector3.up * m_rotSpeed);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("PlatformTest");
+    }
+
+    public void InfoMenu()
+    {
+        m_howToPlay.SetActive(!m_howToPlay.activeSelf);
+    }
+}
