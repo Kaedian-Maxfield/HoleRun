b12360fc098e1530531a98a4d87351f60a9d9aa6
Neko Anstle <nekoanstle@gmail.com>
2019-05-23 09:50:36 -0600

    Sounds Added
    Button sound effects for the main menu added as well as music for both the main menu and the game has been added

---
+++ Assets/btnFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFX : MonoBehaviour
+{
+    public AudioSource m_hoverSound;
+    public AudioClip m_hoverFx;
+    public AudioClip m_clickFx;
+
+    public void HoverSound()
+    {
+        m_hoverSound.PlayOneShot(m_hoverFx);
+    }
+    public void ClickSound()
+    {
+        m_hoverSound.PlayOneShot(m_clickFx);
+    }
+}
